365f0660e33d4091eb6b45d15caafdd14f8dafe1
EmamulArefin <22-46608-1@student.aiub.edu>
2025-05-14 22:03:16 +0600

    try to fix errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { ConfigModule } from '@nestjs/config';
   }), 
     TypeOrmModule.forRoot({
       type: process.env.DB_TYPE as any,
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
