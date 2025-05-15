121b839312b0b81dbd3dc9f6f9545e5a8545c7ba
EmamulArefin <22-46608-1@student.aiub.edu>
2025-05-15 09:48:09 +0600

    Version 0.2

--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,8 @@ async function bootstrap() {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        secure: true,
-        sameSite: 'none',
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       },
     }),
   );
